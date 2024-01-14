2c05bd5d3aaa97379d5797553a598f817090b8b8
Gaurav Shresth <gaurav.shresth2000@gmail.com>
2024-01-14 10:24:18 +0530

    db changed

--- db.js
+++ db.js
@@ -3,12 +3,11 @@ require('dotenv').config()
 // const mongostr = "mongodb://localhost:27017/SMS";
 const mongostr = process.env.MONGO_STR2;
 
-const connectToMongo = () => {
-    mongoose
-        .connect(mongostr, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+const connectToMongo = async () => {
+    await mongoose.connect(mongostr, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
         .then(console.log("Connected to MongoDB"))
         .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
 }
